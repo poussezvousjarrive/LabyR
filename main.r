99f3ad91ae679f3dc9c0062d29fc02255bd6d2a4
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-11-09 14:44:54 +0000

    added standard drawing structure for testing purposes

--- main.r
+++ main.r
@@ -1,21 +1,28 @@
 library(TurtleGraphics)
+
 source('classes/Loggerhead.r')
+source('classes/Path.r')
+source('classes/Polygon.r')
 
 # Exemple d'utilisation
 turtle <- Loggerhead$new()
 
 # Définir un carré de côté 1
-square <- Polygon$fromVertices(list(
-  c(0, 0),
-  c(1, 0),                     
-  c(1, 1),
-  c(0, 1)
+path <- Path$new()
+
+for (i in 1:4) {
+  path$forward(50)
+  path$turn(90)
+}
+
+square <- Polygon$new(path)
+
+# Ajouter un calque contenant le carré
+turtle$addLayer()
+
+turtle$buildShapes(list(
+  square
 ))
 
-# Ajouter un layer contenant un carré
-turtle$addLayer(list(
-   square
-))
-
-# Afficher le dernier layer
-turtle$display()
+# Afficher le dernier calque
+# turtle$display()
